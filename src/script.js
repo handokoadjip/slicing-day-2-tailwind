99d73b21172ccaf37a1d8a0eed598385c4afb42c
handokoadjipangestu <handokoadjipangestu@gmail.com>
2022-06-22 19:56:08 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("#toggle-menu-button"),
+    menu = document.querySelector("#toggle-menu"),
+    open = document.querySelector("#open"),
+    close = document.querySelector("#close"),
+    nav = document.querySelector("#nav");
+
+  function mobileViewUpdate() {
+    const viewportWidth = window.screen.width;
+    if (viewportWidth <= 768) {
+      nav.classList.add("scrolled");
+    } else {
+      document.addEventListener("scroll", function () {
+        nav.classList.toggle(
+          "scrolled",
+          document.scrollingElement.scrollTop > nav.style.height
+        );
+      });
+    }
+  }
+
+  button.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    open.classList.toggle("hidden");
+    close.classList.toggle("hidden");
+  });
+
+  window.addEventListener("load", mobileViewUpdate);
+  window.addEventListener("resize", mobileViewUpdate);
+
+  mobileViewUpdate();
+});
